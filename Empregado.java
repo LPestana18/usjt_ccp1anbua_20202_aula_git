ef4d939c13b153b8229c4fa6a961a5a45746229e
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-03 20:36:44 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,23 @@
 public class Empregado {
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    private double calculaSalario() {
+        switch(tipo) {
+            case 1:
+                return salario;
+            case 2:
+                return salario + salario * comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
